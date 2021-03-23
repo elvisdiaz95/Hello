5759a216ba85aaae5fc37860d80d00fec3009dfc
elvisdiazsoftfolio <74681800+elvisdiazsoftfolio@users.noreply.github.com>
2021-03-23 17:54:48 +0100

    Update Hello.java

--- src/com/example/hello/Hello.java
+++ src/com/example/hello/Hello.java
@@ -4,14 +4,11 @@ import java.util.Scanner;
 
 public class Hello {
     public static void main(String[] args) {
-
-        //Las lineas en blanco mas de una se ve raro. Deja una sola si quieres dividir algo.
         Scanner scanner = new Scanner(System.in);
-        //Las variables siempre ponle un valor inicial. String name = "". int age = 0 ....
-        String name;
-        int age;
-        String city;
-
+        String name = "";
+        int age = 0;
+        String city = "";
+        
         System.out.println("Hello. What is your name?");
         name = scanner.nextLine();
         System.out.println("It's nice to meet you, " + name + ". How old are you?");
@@ -20,10 +17,5 @@ public class Hello {
         System.out.println("Where do you live?");
         city = scanner.next();
         System.out.println("Wow! I've always wanted to go to " + city + ". Thanks for chatting with me. Bye!");
-
-
-
-
-        //Las lineas en blanco mas de una se ve raro. Deja una sola si quieres dividir algo.
     }
 }
